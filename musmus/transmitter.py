6752d57c78c91899c64caa95f5fd4a3a81e41b7e
agamemnonc <agamemnon.krasoulis@gmail.com>
2019-11-29 14:09:11 +0000

    midi channel

--- musmus/transmitter.py
+++ musmus/transmitter.py
@@ -5,26 +5,34 @@ __all__ = ['Transmitter']
 
 
 class Transmitter(object):
-    def __init__(self, midi_port, note_x=60, note_y=61, note_snap=62):
+    def __init__(self, midi_port, channel=1, note_x=60, note_y=61, note_snap=62):
 
         self.midi_port = midi_port
+        self.channel = channel
         self.note_x = note_x
         self.note_y = note_y
         self.note_snap = note_snap
 
-        self.port = mido.open_output(self.midi_port)
+        self._init()
+
+    def _init(self):
+        self.port_ = mido.open_output(self.midi_port)
+        # Midi port numbering uses 1-based indexing
+        self.channel_ = self.channel - 1
 
     def midi_mapping(self):
         for note, function in zip(
                [self.note_x, self.note_y, self.note_snap],
                ['X', 'Y', 'Snap number']):
             wait = input("Triger {} midi mapping key.".format(function))
-            msg = mido.Message('note_on', note=note, velocity=1)
+            msg = mido.Message('note_on', channel=self.channel_, note=note,
+                               velocity=1)
             self._send_msg(msg)
 
     def set_position(self, x, y):
         for velocity, note in zip([x, y], [self.note_x, self.note_y]):
-            msg = mido.Message('note_on', note=note, velocity=velocity)
+            msg = mido.Message('note_on', channel=self.channel_, note=note,
+                               velocity=1)
             self._send_msg(msg)
 
     def set_snap(self, snap):
@@ -33,10 +41,10 @@ class Transmitter(object):
         self._send_msg(msg)
 
     def _send_msg(self, msg):
-        self.port.send(msg)
+        self.port_.send(msg)
 
     def stop(self):
-        self.port.close()
+        self.port_.close()
 
     def __del__(self):
         self.stop()
